8d3938e16478b106e33d5fbaf0cd50e4e88a5911
HyuNagan <134360140+HyuNagan@users.noreply.github.com>
2025-04-26 16:58:55 +0700

    Update

--- obj/Release/net8.0-windows/COMP1551.AssemblyInfo.cs
+++ obj/Release/net8.0-windows/COMP1551.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("COMP1551")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76a59e142a8204ccef5d545e123509a9ceb6dd25")]
 [assembly: System.Reflection.AssemblyProductAttribute("COMP1551")]
