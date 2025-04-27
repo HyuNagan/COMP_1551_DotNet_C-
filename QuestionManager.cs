275008c28388e95f21360ffbd4e7286dd3bfc4cd
HyuNagan <134360140+HyuNagan@users.noreply.github.com>
2025-04-27 15:59:18 +0700

    implemented showing data on datagridview

--- QuestionManager.cs
+++ QuestionManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using COMP1551.DBContext;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace COMP1551
 {
@@ -15,6 +18,48 @@ namespace COMP1551
         public QuestionManager()
         {
             InitializeComponent();
+            this.Load += new EventHandler(QuestionManager_Load); // Đăng ký sự kiện Load
+
+        }
+
+        // Sự kiện Load để gọi LoadQuestions khi form được tải
+        private void QuestionManager_Load(object sender, EventArgs e)
+        {
+            LoadQuestions();
+        }
+
+        private void LoadQuestions()
+        {
+            try
+            {
+     
+                using (var context = new QuizDbContext())
+                {
+                    // Truy vấn dữ liệu từ bảng Questions và chuyển thành danh sách đối tượng
+                    var questions = context.Questions
+                                           .Select(q => new
+                                           {
+                                               q.Id,
+                                               q.Text,
+                                               q.Type,
+                                               q.OptionA,
+                                               q.OptionB,
+                                               q.OptionC,
+                                               q.OptionD,
+                                               q.Answer
+                                           })
+                                           .ToList();
+
+                    // Gán dữ liệu vào DataGridView
+                    dataGridView1.DataSource = questions;
+                }
+            }
+            catch (Exception ex)
+            {
+                // Hiển thị thông báo nếu có lỗi
+                MessageBox.Show("Đã xảy ra lỗi: " + ex.Message);
+            }
         }
     }
+
 }
